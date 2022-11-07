963994cb082fc2febf6cca11b0c90d4cc38cfb2a
David Celis <davidcelis682@gmail.com>
2022-11-07 13:32:36 -0500

    Funcionalidad inicio de sesion

--- src/util/keys.js
+++ src/util/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'postgres://jmmtujtw:F4thsJS2amvcAkUsRV09rcfQ3Y2-caIv@peanut.db.elephantsql.com/jmmtujtw',
-    DB2: 'postgres://dsinftzq:Hks7_GAmrK2mHAndtUKwhBraG-qy79Hy@castor.db.elephantsql.com/dsinftzq'
+    DB: 'postgres://jmmtujtw:F4thsJS2amvcAkUsRV09rcfQ3Y2-caIv@peanut.db.elephantsql.com/jmmtujtw'
 }
