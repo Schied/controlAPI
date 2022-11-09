fdc3b1f7dbf197abda9efdf5e8f07ced97aace5f
David Celis <davidcelis682@gmail.com>
2022-11-09 18:20:45 -0500

    Update

--- src/routes/recurso.js
+++ src/routes/recurso.js
@@ -6,7 +6,7 @@ const upload = multer({ dest: os.tmpdir() });
 
 router.post('/', upload.single('file'), recursoCtrl.subir);
 router.get('/', recursoCtrl.getAll);
-router.get('/ver/:name', recursoCtrl.funcionaporfavor);
+router.get('/ver/:name', recursoCtrl.verPDF);
 router.get('/:Id_equipo', recursoCtrl.getByEquipo);
 router.put('/', recursoCtrl.actualizarRecurso);
 
