89a8aa75c3175afad475d1b4758fe8c1872c0709
David Celis <davidcelis682@gmail.com>
2022-11-10 10:03:02 -0500

    Update

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -5,5 +5,5 @@ const usuarioCtrl = require('../controllers/usuarioCtrl');
 
 router.post('/', usuarioCtrl.createUser);
 router.post('/auth/', usuarioCtrl.find, usuarioCtrl.signin);
-
+router.post('/verifyToken', authCtrl.verifyToken);
 module.exports = router;
